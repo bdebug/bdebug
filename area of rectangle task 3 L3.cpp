3bdc22e7c497cc7fec6177a3fc5697e773a3bdc8
bdebug <bismanawaz13@gmail.com>
2025-03-05 03:00:10 +0500

    to calculate the area of a rectangle

---
+++ area of rectangle task 3 L3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	float length;
+	float width;
+	float area;
+	
+	cout<<"enter the length of rectangle:";
+	cin>>length;
+	
+	cout<<"enter the width of rectangle:";
+	cin>>width;
+	
+	cout<<"area of rectangle:"<<length*width;
+	
+	return 0 ;
+	
+}
